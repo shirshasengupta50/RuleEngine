2fbf2ca6eb412c28ab4c38e547c739b3bc153c0e
Shirsha Sengupta <devofshirsha@gmail.com>
2024-10-23 22:55:10 +0530

    [fix] : Fix modify rule

--- server/src/utils/helper/modificationAST.js
+++ server/src/utils/helper/modificationAST.js
@@ -1,14 +1,25 @@
 const updateValue = (node, fieldName, newValue) => {
-
-    if (node.type === 'condition') {
+    let fieldFound = false; // Track if the fieldName was found
+  
+    const traverseAndUpdate = (node) => {
+      if (node.type === 'condition') {
         if (node.left.field === fieldName) {
-            node.right.value = newValue; 
+          node.right.value = newValue;
+          fieldFound = true; // Set to true if the fieldName is found and updated
         }
-    } else if (node.type === 'operator') {
-        if (node.left) updateValue(node.left, fieldName, newValue);
-        if (node.right) updateValue(node.right, fieldName, newValue);
+      } else if (node.type === 'operator') {
+        if (node.left) traverseAndUpdate(node.left);
+        if (node.right) traverseAndUpdate(node.right);
+      }
+    };
+  
+    traverseAndUpdate(node);
+  
+    if (!fieldFound) {
+      throw new Error(`Field "${fieldName}" not found in the AST.`);
     }
 }
+  
 
 const changeOperator = (node, newOperator) => {
 
